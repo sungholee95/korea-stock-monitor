ed432e730b5a1b8e21f003a07b648cac17102b29
Sung-Ho Lee <sungholee95@gmail.com>
2026-03-19 18:55:43 +0900

    feat(client): Add KISClient class for REST communication (WebSocket WIP)

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,9 +1,11 @@
 from .adapters.kis.auth import KISAuth
+from .adapters.kis.client import KISClient
 from .adapters.kis.config import KISConfig
 from .adapters.kis.endpoints import KISEndpoint
 
 __all__ = [
     "KISAuth",
+    "KISClient",
     "KISConfig",
     "KISEndpoint",
 ]
--- monitor/adapters/kis/__init__.py
+++ monitor/adapters/kis/__init__.py
@@ -1,9 +1,11 @@
 from .auth import KISAuth
+from .client import KISClient
 from .config import KISConfig
 from .endpoints import KISEndpoint, get_endpoint_spec
 
 __all__ = [
     "KISAuth",
+    "KISClient",
     "KISConfig",
     "KISEndpoint",
     "get_endpoint_spec",
