532e29bf6d9357b687cddb00a7ce5b5945066491
Sung-Ho Lee <sungholee95@gmail.com>
2026-05-08 15:28:21 +0900

    archive(kis)!: remove kis from adapters and stop development

--- ksmonitor/__init__.py
+++ ksmonitor/__init__.py
@@ -1,17 +1,9 @@
-from .adapters.kis.auth import KISAuth
-from .adapters.kis.client import KISClient
-from .adapters.kis.config import KISConfig
-from .adapters.kis.endpoints import KISEndpoint
 from .adapters.kiwoom.auth import KiwoomAuth
 from .adapters.kiwoom.client import KiwoomClient
 from .adapters.kiwoom.config import KiwoomConfig
 from .adapters.kiwoom.endpoints import KiwoomEndpoint
 
 __all__ = [
-    "KISAuth",
-    "KISClient",
-    "KISConfig",
-    "KISEndpoint",
     "KiwoomAuth",
     "KiwoomClient",
     "KiwoomConfig",
